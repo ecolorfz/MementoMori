25168ff4e1e6d00230ba89124f90683d3801f620
ecolorfz <minghaozheng53@gmail.com>
2023-12-12 10:58:32 +0800

    Dec12

--- leetcode/sort/solution.cpp
+++ leetcode/sort/solution.cpp
@@ -118,8 +118,6 @@ public:
     给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数。
 
 */
-
-#include <string>
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
@@ -127,23 +125,42 @@ public:
         for(int i=0;i<nums.size();i++){
             snums.push_back(to_string(nums[i]));
         }
-        sort(snums.begin(),snums.end());
+        sort(snums.begin(),snums.end(),[](const string &a,const string &b){
+            return a+b>b+a;
+        });//自定义比较函数，如果ab>ba，那么a>b,onst string & 作为参数避免修改
+        if(snums[0]=="0") return "0";
         string res = "";
-        if(snums.size()==1)return snums[0];
-        for(int i=snums.size()-1;i>0;i-=1){
-            string tmp1 = snums[i] + snums[i-1];
-             
-            string tmp2 = snums[i-1] + snums[i];
-            
-            
-            if(tmp1>tmp2)res+=snums[i];
-            else {
-                if(snums[i]==snums[i-1]&&snums[i]=="0"&&res=="")continue;
-                res+=snums[i-1];
-                snums[i-1] = snums[i];
+        for(string s:snums)res+=s;
+        return res;
+    }
+};
+
+/*
+    75. 颜色分类
+    给定一个包含红色、白色和蓝色，一共 n 个元素的数组，
+    原地对它们进行排序，使得相同颜色的元素相邻，
+    并按照红色、白色、蓝色顺序排列。
+    此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+*/
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+         int p = 0, q = n - 1;
+        for (int i = 0; i <= q; ++i) {
+            if (nums[i] == 0) {
+                nums[i] = nums[p];
+                nums[p] = 0;
+                ++p;
+            }
+            if (nums[i] == 2) {
+                nums[i] = nums[q];
+                nums[q] = 2;
+                --q;
+            i--;
             }
         }
-        res+=snums[0];
-        return res;
+        return;
+ 
     }
 };
